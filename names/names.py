fd55fc92841eeb340f7b8896b9f82b67515f2e85
Liam Cox <liamcox@Liams-MacBook-Pro.local>
2020-08-14 12:36:34 -0500

    mvp

--- names/names.py
+++ names/names.py
@@ -78,9 +78,15 @@ data1 = BSTNode(names_1)
 data1.insert(names_2)
 data1.Print_Tree()
 
-# end_time = time.time()
+
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+end_time = time.time()
 # print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-# print(f"runtime: {end_time - start_time} seconds")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
